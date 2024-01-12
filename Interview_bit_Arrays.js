3881c7a0cd64d33a0f619bb70c05daeca4e6703f
Developersunil001 <sunilkumar088839@gmail.com>
2024-01-13 00:10:23 +0530

    arrays

---
+++ Interview_bit_Arrays.js
@@ -0,0 +1,58 @@
+// How to find duplicate elements in an given array
+// 5 ways to remove duplicate in javascript
+
+// sets,filter,forEach,reduce,sort,includes,
+// How to find max and min value in an given array
+// How to find second largest value and remove first largest value in an array
+// How to find the missing number in a given integer array of 1 to 10
+// How to find odd or Even in an given array
+// How to find the sum of all elements in an given array
+// How to find factorial(n!) of a given number
+// How to find prime no in js
+// How to find palindrome in js
+// How to swap two variable without using 3rd variable
+// How to merge two arrays and sort them in js
+// How to find factors of a given integers
+// How to make calculator in js
+// How to compare two arrays are equal or not in js
+// How to find intersection of two arrays/sets in js
+// How to find union of two arrays in js
+// How to convert celcius to Farenheit or vice versa
+// How to convert km into miles in js
+// Program to find fibonacci sequence in js
+// Program to check armstrong number in js
+// Print All Distinct Elements of a given array
+// Filtering numbers out from an array
+// Return Only numbers from Array
+// JavaScript multiply each array element with Array length
+// count occurrences of each element in an array
+// Count number of duplicate Elements in Array
+// How can I remove a specific item from an array
+// How to Reverse Arrays in JavaScript Without Using reverse()
+// Even and Odd Position Array elements in JS
+// Finding average and sum of Array elements using Javascript ES6
+// Multiple Ways to Print All Array Elements
+// length of array in JavaScript without using length method
+// How to find the count of duplicates in an array?
+// How to check if a given number is an integer?
+// Sort a given array of strings
+// How to find unique values in an array?
+// How to find unique values from an Array in sorted order?
+// Find maximum value in a numbered array?
+// Find minimum value in a numbered array?
+// Find the average of the numbers in the numbered array?
+// How can you uppercase the first character in a string array?
+// How to make a sentence out of the given string array?
+// How to check if an array contains any element of another array?
+// Given an array of strings, reverse each word in the sentence?
+// How to check if an object is present in an Array or not?
+// How do you sort and reverse an array without changing the original arry?
+// How can you combine two Arrays into a third Array using spread operator
+// How do you add an element at the beginning of an array?
+// How to remove an element from the end of the array?
+// How can you split a string into an Array?
+// How do you create an Array out of a given sentence?
+// How do you replace a given string in the string of arrays?
+// How do you find the Vowels?
+// How to calculate the length of an associative array using JavaScript?
+// How to generate fibonacci series in JavaScript?
